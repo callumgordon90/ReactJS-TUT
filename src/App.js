a2edd25c3c975108ac5bd3938abe3274296ff6d2
callum <callumjosephiaingordon@gmail.com>
2025-01-15 15:51:43 +0100

    added modifications to game component

--- src/App.js
+++ src/App.js
@@ -99,16 +99,21 @@ export default function Game(){
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
 
+  const [currentMove, setCurrentMove] = useState(0);
+
   const currentSquares = history[history.length - 1];
 
     function handlePlay(nextSquares){
-      //TODO
-      setHistory([...history, nextSquares]);
+      const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+      setHistory(nextHistory);
+      setCurrentMove(nextHistory.length - 1);
+
       setXIsNext(!xIsNext);
     }
 
     function jumpTo(nextMove){
-      //TODO
+      setCurrentMove(nextMove);
+      setXIsNext(nextMove % 2 === 0);
     }
 
     const moves = history.map((squares, move) =>{
@@ -119,7 +124,7 @@ export default function Game(){
         description = 'Go to game start';
       }
       return(
-        <li>
+        <li key={move}>
           <button onClick={()=> jumpTo(move)}>{description}</button>
         </li>
       );
