e5cf8383d250024b00be00ace6b3ddec943d5e9e
callum <callumjosephiaingordon@gmail.com>
2025-01-15 15:39:23 +0100

    added game elements

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import "./styles.css";
 import App from './App';
 import Square from './App';
+import Game from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Square />);
+root.render(<Game/>);
